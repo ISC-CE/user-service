717c230ca67c713d2b311000d493e14420c649e7
Ashwin Raam <raamocyby@gmail.com>
2023-11-06 11:07:25 -0500

    UPDATES database connection and models

--- models/UserAddress.model.js
+++ models/UserAddress.model.js
@@ -1,22 +1,5 @@
-const config = require("../configs/db.config.js");
-
-const { Sequelize, DataTypes } = require('sequelize');
-
-const sequelize = new Sequelize(
-    config.DB,
-    config.USER,
-    config.PASSWORD,
-    {
-        host: config.HOST,
-        dialect: config.dialect,
-        pool: {
-            max: config.pool.max,
-            min: config.pool.min,
-            acquire: config.pool.acquire,
-            idle: config.pool.idle
-        }
-    }
-);
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database/sequelize');
 
 const UserAddress = sequelize.define('Address', {
     addressId: {
